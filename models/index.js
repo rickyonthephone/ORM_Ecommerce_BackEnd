34f89e382e81f29ba36ceee3c1801d3413ea3d4c
Richard Crippen <richard.crippen@gmail.com>
2021-05-13 13:22:04 -0400

    building relationshiops in index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  // Define the third table needed to store the foreign keys
+  foreignKey: category_id
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreginKey: category_id
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreginKey: category_id
+  }
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreginKey: category_id
+  }
+});
 
 module.exports = {
   Product,
